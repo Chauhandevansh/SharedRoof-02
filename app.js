cd5a90867e66197c1f8daac2f43a175202e87456
Chauhandevansh <69097279+Chauhandevansh@users.noreply.github.com>
2021-12-28 20:16:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var fs = require('fs');
 var path = require('path');
 const bcrypt = require('bcryptjs')
 
-mongoose.connect("mongodb://localhost:27017/Room_Share", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://devansh01:devdev30@cluster0.rrnxk.mongodb.net/shared-roof?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connection Successfull...."))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("Connection unSuccessfull...."));
 
 // Step 4 - set up EJS
 
@@ -268,7 +268,7 @@ app.post("/login", async(req, res) => {
 
 
 
-var port = process.env.PORT || '3000'
+var port = process.env.PORT || '5000'
 app.listen(port, err => {
     if (err)
         throw err
